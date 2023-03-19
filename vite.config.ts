59691b051e5c746dfa39afa6914d22b17b848afe
Nikita Koshelev <nkoshelev@icloud.com>
2023-03-19 16:23:19 +0300

    build: add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
+import path from 'path'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import { VitePluginFonts } from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets')
+    }
+  },
   plugins: [
     react(),
     VitePluginFonts({
